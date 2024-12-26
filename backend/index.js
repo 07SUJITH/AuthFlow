05e4581f076507ba65dc3b93818c3a690e4156af
07SUJITH <tssujith2002@gmail.com>
2024-12-26 18:00:12 +0530

    ✨feat : Implement CORS middleware and serve static files in production

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,10 @@ import express from "express";
 import { connectDB } from "./db/connectDB.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 import authRouter from "./routes/auth.route.js";
+import path from "path";
+
 /**
  * Load environment variables from .env file.
  */
@@ -16,6 +18,9 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve(); // Define the current directory path
+// Cross-Origin Resource Sharing (CORS) is a security feature that allows servers to specify who can access their resources.
+app.use(cors({ origin: "http://localhost:5173", credentials: true })); // Middleware to enable CORS
 app.use(express.json()); // Middleware to parse incoming requests: req.body  JSON data. (json to object)
 app.use(cookieParser()); // Middleware to parse incoming cookies from the client. (cookie to object)
 /**
@@ -46,6 +51,17 @@ startServer();
  * @memberof module:index
  */
 app.use("/api/auth", authRouter);
+
+// Serve static files in production
+if (process.env.NODE_ENV === "production") {
+  // Serve static files from the "frontend/dist" directory
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  // Handle all other routes by serving the "index.html" file
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 /**
  * Root route handler.
